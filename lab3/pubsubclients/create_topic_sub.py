b9bbcd40ec70d3f212a03529a4e0fc4b3c15dbf7
MaxMaas <113433909+MaxMaas@users.noreply.github.com>
2023-02-16 11:54:17 +0100

    lab 3 id edits

--- lab3/pubsubclients/create_topic_sub.py
+++ lab3/pubsubclients/create_topic_sub.py
@@ -5,3 +5,5 @@ if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     create_topic("ada2023-376707", "diabetes_req")   # replace ada2023 with your project id
     create_topic("ada2023-376707", "diabetes_res") # replace ada2023 with your project id
+
+
